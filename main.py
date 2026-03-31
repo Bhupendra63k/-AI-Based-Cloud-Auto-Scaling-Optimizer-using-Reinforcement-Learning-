11a0c58df4746355235dbf4f01f5f6e5d7432cf2
Akanshanirwan12 <nirwanakansha.23@gmail.com>
2026-03-31 21:10:52 +0530

    add file

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ env = CloudEnv()
 class ActionInput(BaseModel):
     action: int
 
+@app.get("/")
+def root():
+    return {"message": "Cloud Auto Scaling Env Running"}
+
 @app.post("/reset")
 async def reset():
     state = env.reset()
-    return {
-        "observation": list(state)   # 🔥 FIX HERE
-    }
+    return {"observation": list(state)}
 
 @app.post("/step")
 async def step(input: ActionInput):
     state, reward, done, info = env.step(input.action)
     return {
-        "observation": list(state),  # 🔥 FIX HERE
+        "observation": list(state),
         "reward": float(reward),
         "done": bool(done),
         "info": info
